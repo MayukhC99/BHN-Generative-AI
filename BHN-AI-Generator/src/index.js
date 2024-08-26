65ead012c8d27ed4cbd70651416827fc00b9c6ef
Prasad Mudgal <prasad.mudgal@bhnetwork.com>
2024-08-27 00:08:02 +0530

    Modified presentation layer

--- BHN-AI-Generator/src/index.js
+++ BHN-AI-Generator/src/index.js
@@ -6,6 +6,10 @@ import {
     Form, Row, Col, Input,
     Card, CardBody, CardTitle, CardImg, CardGroup
 } from 'reactstrap';
+
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 import { v4 as uuidv4 } from 'uuid';
 
 import base64s from './base64';
@@ -20,9 +24,12 @@ function DataURIToBlob(dataURI) {
         ia[i] = byteString.charCodeAt(i)
 
     return new Blob([ia], { type: mimeString })
-  }
+}
 
-const AIWidgetModal = ({ modalStatus, setModalStatus, urlCallback }) => {
+const AIWidgetModal = ({ modalStatus, setModalStatus, urlCallback, howToUseGif }) => {
+
+    const [isLoadingImage, setIsLoadingImage] = useState(true);
+    const [retryCount, setRetryCount] = useState(0);
 
     const toggleModal = () => { setModalStatus(!modalStatus) };
     const likedAnImage = id => async () => {
@@ -34,7 +41,7 @@ const AIWidgetModal = ({ modalStatus, setModalStatus, urlCallback }) => {
 
         const file_name = `AI_generated_${uuidv4()}.png`;
         bodyFormData.append(file_name, fileToUpload, file_name);
-    
+
         const options = {
             method: 'POST',
             body: bodyFormData
@@ -48,7 +55,7 @@ const AIWidgetModal = ({ modalStatus, setModalStatus, urlCallback }) => {
             }
             const result = await response.json();
 
-             // Extract value from the JSON object
+            // Extract value from the JSON object
             // Assuming there is only one key-value pair in the response
             const [_, fileId] = Object.entries(result)[0];
             const url = `https://ucarecdn.com/${fileId}/`;
@@ -57,6 +64,19 @@ const AIWidgetModal = ({ modalStatus, setModalStatus, urlCallback }) => {
             toggleModal(false);
         } catch (error) {
             console.error(error);
+        }
+    };
+
+    const generateImageSearchHandler = (ev) => {
+        if (retryCount != 3) {
+            const imageSearchInput = document.getElementById("search-box");
+            console.log('searchValue : ', imageSearchInput.value);
+
+            setTimeout(() => {
+                setIsLoadingImage(false);
+            }, 3000);
+
+            setRetryCount(retryCount + 1);
         }
     };
 
@@ -76,77 +96,168 @@ const AIWidgetModal = ({ modalStatus, setModalStatus, urlCallback }) => {
                             <Col sm='12'>
                                 <Input
                                     id="search-box"
-                                    name="serach"
-                                    placeholder="Flower"
+                                    name="searchTextForAIGenerate"
+                                    placeholder="eg. First day of school, 21st Birthday of my son"
                                     type="text"
                                     className='input-search-box'
+                                    disabled={retryCount == 2 && `disabled`}
                                 />
-                                <span className='input-search-button'>
+                                <span
+                                    disabled={retryCount == 2 && `disabled`}
+                                    className='input-search-button'
+                                    onClick={(e) => { generateImageSearchHandler(e) }}
+                                >
                                     <svg width="40px" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M3.33789 7C5.06694 4.01099 8.29866 2 12.0001 2C17.5229 2 22.0001 6.47715 22.0001 12C22.0001 17.5228 17.5229 22 12.0001 22C8.29866 22 5.06694 19.989 3.33789 17M12 16L16 12M16 12L12 8M16 12H2" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>                                </span>
+                                    </svg>
+                                </span>
+                                {
+                                    retryCount == 2 && (<div style={{ color: 'red' }}>Attempts Exhausted: You've used all 2 attempts. Please try and upload your own image.
+                                    </div>)
+                                }
                             </Col>
                         </Row>
 
                     </Form>
-                    <h5 className="pick-one-image-text">Please pick one image from those listed below.</h5>
-                    <CardGroup>
-                        <Card className="card-image-wrapper" onClick={likedAnImage(0)}>
-                            <CardImg
-                                alt="Card image cap"
-                                src={`data:image/png;base64, ${base64s[0]}`}
-                                top
-                                width="100%"
-                            />
-                            <CardBody>
-                                <CardTitle tag="h5">
-                                    Like It ?
-                                </CardTitle>
-                            </CardBody>
-                        </Card>
-                        <Card className="card-image-wrapper" onClick={likedAnImage(1)}>
-                            <CardImg
-                                alt="Card image cap"
-                                src={`data:image/png;base64, ${base64s[1]}`}
-                                top
-                                width="100%"
-                            />
-                            <CardBody>
-                                <CardTitle tag="h5">
-                                    Like It ?
-                                </CardTitle>
-                            </CardBody>
-                        </Card>
-                    </CardGroup>
-                    <CardGroup>
-                        <Card className="card-image-wrapper" onClick={likedAnImage(2)}>
-                            <CardImg
-                                alt="Card image cap"
-                                src={`data:image/png;base64, ${base64s[2]}`}
-                                top
-                                width="100%"
-                            />
-                            <CardBody>
-                                <CardTitle tag="h5">
-                                    Like It ?
-                                </CardTitle>
-                            </CardBody>
-                        </Card>
-                        <Card className="card-image-wrapper" onClick={likedAnImage(3)}>
-                            <CardImg
-                                alt="Card image cap"
-                                src={`data:image/png;base64, ${base64s[3]}`}
-                                top
-                                width="100%"
-                            />
-                            <CardBody>
-                                <CardTitle tag="h5">
-                                    Like It ?
-                                </CardTitle>
-                            </CardBody>
-                        </Card>
-                    </CardGroup>
 
+                    {retryCount != 0
+                        ? (
+                            <React.Fragment>
+                                <h5 className="pick-one-image-text">Please pick one image from those listed below.</h5>
+                                <CardGroup>
+                                    <Card className="card-image-wrapper" onClick={likedAnImage(0)}>
+                                        {
+                                            !isLoadingImage
+                                            && (
+                                                <React.Fragment>
+                                                    <CardImg
+                                                        alt="Card image cap"
+                                                        src={`data:image/png;base64, ${base64s[0]}`}
+                                                        top
+                                                        width="100%"
+                                                        className='card-image'
+                                                    />
+                                                    <CardBody>
+                                                        <CardTitle tag="h5">
+                                                            Like It ?
+                                                        </CardTitle>
+                                                    </CardBody>
+                                                </React.Fragment>
+                                            )
+                                        }
+                                        {isLoadingImage
+                                            && (
+                                                <React.Fragment>
+                                                    <SkeletonTheme color="#202020" highlightColor="#c1c1c1">
+                                                        <Skeleton height='100%' width='100%' />
+                                                    </SkeletonTheme>
+                                                </React.Fragment>
+                                            )
+                                        }
+                                    </Card>
+                                    <Card className="card-image-wrapper" onClick={likedAnImage(1)}>
+                                        {
+                                            !isLoadingImage
+                                            && (
+                                                <React.Fragment>
+                                                    <CardImg
+                                                        alt="Card image cap"
+                                                        src={`data:image/png;base64, ${base64s[1]}`}
+                                                        top
+                                                        width="100%"
+                                                        className='card-image'
+                                                    />
+                                                    <CardBody>
+                                                        <CardTitle tag="h5">
+                                                            Like It ?
+                                                        </CardTitle>
+                                                    </CardBody>
+                                                </React.Fragment>
+                                            )
+                                        }
+                                        {isLoadingImage
+                                            && (
+                                                <React.Fragment>
+                                                    <SkeletonTheme color="#202020" highlightColor="#c1c1c1">
+                                                        <Skeleton height='100%' width='100%' />
+                                                    </SkeletonTheme>
+                                                </React.Fragment>
+                                            )
+                                        }
+                                    </Card>
+                                </CardGroup>
+
+                                <CardGroup>
+                                    <Card className="card-image-wrapper" onClick={likedAnImage(2)}>
+                                        {
+                                            !isLoadingImage
+                                            && (
+                                                <React.Fragment>
+                                                    <CardImg
+                                                        alt="Card image cap"
+                                                        src={`data:image/png;base64, ${base64s[2]}`}
+                                                        top
+                                                        width="100%"
+                                                        className='card-image'
+                                                    />
+                                                    <CardBody>
+                                                        <CardTitle tag="h5">
+                                                            Like It ?
+                                                        </CardTitle>
+                                                    </CardBody>
+                                                </React.Fragment>
+                                            )
+                                        }
+                                        {isLoadingImage
+                                            && (
+                                                <React.Fragment>
+                                                    <SkeletonTheme color="#202020" highlightColor="#c1c1c1">
+                                                        <Skeleton height='100%' width='100%' />
+                                                    </SkeletonTheme>
+                                                </React.Fragment>
+                                            )
+                                        }
+                                    </Card>
+                                    <Card className="card-image-wrapper" onClick={likedAnImage(3)}>
+                                        {
+                                            !isLoadingImage
+                                            && (
+                                                <React.Fragment>
+                                                    <CardImg
+                                                        alt="Card image cap"
+                                                        src={`data:image/png;base64, ${base64s[3]}`}
+                                                        top
+                                                        width="100%"
+                                                        className='card-image'
+                                                    />
+                                                    <CardBody>
+                                                        <CardTitle tag="h5">
+                                                            Like It ?
+                                                        </CardTitle>
+                                                    </CardBody>
+                                                </React.Fragment>
+                                            )
+                                        }
+                                        {isLoadingImage
+                                            && (
+                                                <React.Fragment>
+                                                    <SkeletonTheme color="#202020" highlightColor="#c1c1c1">
+                                                        <Skeleton height='100%' width='100%' />
+                                                    </SkeletonTheme>
+                                                </React.Fragment>
+                                            )
+                                        }
+                                    </Card>
+                                </CardGroup>
+                            </React.Fragment>
+                        )
+                        : (<CardGroup className='how-to-use-gif-wrapper'>
+                            <h2 className="pick-one-image-text center">How to use</h2>
+                            <div className='how-to-use-gif'>
+                                <img src={howToUseGif} />
+                            </div>
+                        </CardGroup>)
+                    }
                 </ModalBody>
             </Modal>
         </React.Fragment >
