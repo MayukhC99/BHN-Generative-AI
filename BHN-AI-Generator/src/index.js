428d333389a0b70fe68c7572c4767ec23a286639
Prasad Mudgal <prasad.mudgal@bhnetwork.com>
2024-08-27 01:17:47 +0530

    Modified Presentation layer

--- BHN-AI-Generator/src/index.js
+++ BHN-AI-Generator/src/index.js
@@ -67,6 +67,9 @@ const AIWidgetModal = ({ modalStatus, setModalStatus, urlCallback, howToUseGif }
         }
     };
 
+    const [isLoadingImage, setIsLoadingImage] = useState(true);
+    const [retryCount, setRetryCount] = useState(0);
+
     const generateImageSearchHandler = (ev) => {
         if (retryCount != 3) {
             const imageSearchInput = document.getElementById("search-box");
